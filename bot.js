ca9668dd41ad49d5730501e1bc028a82f7f059a3
RyadForGamessss <53405623+RyadForGamessss@users.noreply.github.com>
2019-07-29 23:30:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,13 +11,13 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "604673228614533146") return;
+  if (message.author.id !== "488746365933846529") return;
 
   
-  if (message.content.startsWith(prefix + 'setwatch')) {
+  if (message.content.startsWith(prefix + 'NeflixSw')) {
   client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
-    message.channel.sendMessage(`Watch Now: **${argresult}`)
+    message.channel.sendMessage(`:white_check_mark: ${argresult}`)
 } 
 
  
@@ -28,7 +28,7 @@ client.on('message', message => {
 } 
 
 
-if (message.content.startsWith(prefix + 'setname')) {
+if (message.content.startsWith(prefix + 'NetflixUc')) {
   client.user.setUsername(argresult).then
       message.channel.sendMessage(`Username Changed To **${argresult}**`)
   return message.reply("You Can change the username 2 times per hour");
@@ -36,15 +36,15 @@ if (message.content.startsWith(prefix + 'setname')) {
 
 if (message.content.startsWith(prefix + 'setavatar')) {
   client.user.setAvatar(argresult);
-   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+   message.channel.sendMessage(`:white_check_mark: ${argresult}**`);
 }
 
-if (message.content.startsWith(prefix + 'RaNia')) {
+if (message.content.startsWith(prefix + 'NetflixSt')) {
   client.user.setGame(argresult, "https://www.twitch.tv/peery13");
      console.log('test' + argresult);
     message.channel.sendMessage(`:white_check_mark: ${argresult}`)
 } 
-if (message.content.startsWith(prefix + 'RaNiaPl')) {
+if (message.content.startsWith(prefix + 'NetflixPl')) {
   client.user.setGame(argresult);
      console.log('test' + argresult);
     message.channel.sendMessage(`:white_check_mark: ${argresult}`)
